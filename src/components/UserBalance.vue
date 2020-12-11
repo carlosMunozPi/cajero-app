3d9c2d7505720fe51fa4fc327ce6b900d68bab4d
carlosMunozPi <caamunozpi@gmail.com>
2020-12-10 20:13:26 -0500

    se agrega componente heroku

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -17,7 +17,7 @@
         created: function(){
             this.username = this.$route.params.username
             let self = this
-            axios.get("http://localhost:8000/users/balance/" + this.username)
+            axios.get("https://backend-cajero-camp.herokuapp.com/user/balance/" + this.username)
                 .then((result) => {
                     self.balance = result.data.balance
                 })
